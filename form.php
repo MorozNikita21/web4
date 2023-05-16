18479d5366a32fc94c42de38b09da077f8b3c0b7
Moroz Nikita <boss.film8600@mail.ru>
2023-05-16 22:47:27 +0300

    Task4

---
+++ form.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <title>Task 4</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id = "form">
+  <form action=""
+    method="POST">
+    <label>
+      Имя:<br />
+      <input name="name"
+      <?php print($errors['name'] ? 'class="error"' : '');?> value="<?php print $values['name'];?>"/>
+    </label><br />
+    <label >
+      E-mail:<br />
+      <input name="email"
+        type="email"
+        <?php print($errors['email'] ? 'class="error"' : '');?> value="<?php print $values['email'];?>"/>
+    </label><br />
+    <label>
+      Год рождения:<br />
+      <select name="birthdayear">
+        <?php 
+        for ($i = 1950; $i <= 2023; $i++) {
+          $selected= ($i == $values['birthdayear']) ? 'selected="selected"' : '';
+          printf('<option value="%d" %s>%d год</option>', $i, $selected, $i);
+        }
+        ?>
+      </select><br />
+      Пол<br/>
+    <label><input type="radio" checked="checked"
+      name="gen" value="m" 
+      <?php print($errors['gen'] ? 'class="error"' : '');?>
+      <?php if ($values['gen']=='m') print 'checked';?>
+      />
+      Мужской</label>
+    <label><input type="radio"
+      name="gen" value="f"
+    <?php print($errors['gen'] ? 'class="error"' : '');?>
+      <?php if ($values['gen']=='f') print 'checked';?>
+    />
+      Женский</label><br />
+      Количество конечностей<br/>
+    <div class="lim">
+    <label><input type="radio" checked="checked"
+      name="body" value="0" 
+    <?php print($errors['body'] ? 'class="error"' : '');?>
+      <?php if ($values['body']=='0') print 'checked';?>
+    />
+      0</label>
+    <label><input type="radio"
+      name="body" value="4" 
+    <?php print($errors['body'] ? 'class="error"' : '');?>
+      <?php if ($values['body']=='4') print 'checked';?>              
+    />
+      4</label><br />
+    <label><input type="radio"
+      name="body" value="5" 
+    <?php print($errors['body'] ? 'class="error"' : '');?>
+      <?php if ($values['body']=='5') print 'checked';?>              
+    />
+      5</label><br />
+    <label>
+      Сверхспособности
+      <br />
+      <select name="ability[]"
+          multiple="multiple" <?php print($errors['ability'] ? 'class="error"' : '');?>>
+          <option value="1" <?php print(in_array('1', $values['ability']) ? 'selected ="selected"' : '');?>>бессмертие</option>
+          <option value="2" <?php print(in_array('2', $values['ability']) ? 'selected ="selected"' : '');?>>прохождение сквозь стены</option>
+          <option value="3" <?php print(in_array('3', $values['ability']) ? 'selected ="selected"' : '');?>>левитация</option>
+          <option value="4" <?php print(in_array('4', $values['ability']) ? 'selected ="selected"' : '');?>>не чувствовать боль</option>
+      </select>
+      </label><br />
+      <label>
+      Биография<br />
+        <textarea name="biographiya"
+        <?php print($errors['biographiya'] ? 'class="error"' : '');?>
+        value = "<?php print $values['biographiya'];?>"></textarea>
+        </label><br />    
+      <label><input type="checkbox" checked="checked"
+        name="check" />
+        Ознакомился с контрактом</label>
+      <input type="submit" value="Отправить" />
+    </form>
+  </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,145 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  $messages = array();
+
+  if (!empty($_COOKIE['save'])) {
+    setcookie('save', '', 100000);
+    $messages[] = 'Результаты были сохранены';
+  }
+
+  $errors = array();
+  $errors['name'] = !empty($_COOKIE['name_error']);
+  $errors['email'] = !empty($_COOKIE['email_error']);
+  $errors['birthdayear'] = !empty($_COOKIE['birthdayear_error']);
+  $errors['gen'] = !empty($_COOKIE['gen_error']);
+  $errors['body'] = !empty($_COOKIE['body_error']);
+  $errors['ability'] = !empty($_COOKIE['ability_error']);
+  $errors['biographiya'] = !empty($_COOKIE['biographiya_error']);
+
+  if ($errors['name']) {
+    setcookie('name_error', '', 100000);
+    $messages[] = '<div class="error">Как вас зовут?</div>';
+  }
+  if ($errors['email']) {
+    setcookie('email_error', '', 100000);
+    $messages[] = '<div class="error">Напишить свой e-mail</div>';
+  }
+  if ($errors['birthdayear']) {
+    setcookie('birthdayear_error', '', 100000);
+    $messages[] = '<div class="error">В каком году вы родились?</div>';
+  }
+  if ($errors['gen']) {
+    setcookie('gen_error', '', 100000);
+    $messages[] = '<div class="error">Какого вы пола?</div>';
+  }
+  if ($errors['body']) {
+    setcookie('body_error', '', 100000);
+    $messages[] = '<div class="error">Сколько у вас конечностей?</div>';
+  }
+  if ($errors['ability']) {
+    setcookie('ability_error', '', 100000);
+    $messages[] = '<div class="error">Какие бы вы хотели суперспособности?</div>';
+  }
+  if ($errors['biographiya']) {
+    setcookie('biographiya_error', '', 100000);
+    $messages[] = '<div class="error">Напишите про себя</div>';
+  }
+
+  $values = array();
+  $values['name'] = empty($_COOKIE['name_value']) ? '' : $_COOKIE['name_value'];
+  $values['email'] = empty($_COOKIE['email_value']) ? '' : $_COOKIE['email_value'];
+  $values['birthdayear'] = empty($_COOKIE['birthdayear_value']) ? '' : $_COOKIE['birthdayear_value'];
+  $values['gen'] = empty($_COOKIE['gen_value']) ? '' : $_COOKIE['gen_value'];
+  $values['body'] = empty($_COOKIE['body_value']) ? '' : $_COOKIE['body_value'];
+  $values['ability'] = empty($_COOKIE['ability_value']) ? array() : json_decode($_COOKIE['ability_value']);
+  $values['biographiya'] = empty($_COOKIE['biographiya_value']) ? '' : $_COOKIE['biographiya_value'];
+
+  include('form.php');
+  exit();
+}
+
+$errors = FALSE;
+if (empty($_POST['name'])) {
+  setcookie('name_error', '1', time() + 24 * 60 * 60);
+  $errors = TRUE;
+}
+else {
+  setcookie('name_value', $_POST['name'], time() + 30 * 24 * 60 * 60);
+}
+
+if (empty($_POST['email']) || !preg_match('/^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i', $_POST['email'])) {
+  setcookie('email_error', '1', time() + 24 * 60 * 60);
+  $errors = TRUE;
+}
+else {
+  setcookie('email_value', $_POST['email'], time() + 30 * 24 * 60 * 60);
+}
+
+if (empty($_POST['birthdayear']) || !is_numeric($_POST['birthdayear']) || !preg_match('/^\d+$/', $_POST['birthdayear'])) {
+  setcookie('birthdayear_error', '1', time() + 24 * 60 * 60);
+  $errors = TRUE;
+}
+else {
+  setcookie('birthdayear_value', $_POST['birthdayear'], time() + 30 * 24 * 60 * 60);
+}
+
+if (empty($_POST['gen']) || ($_POST['gen']!='m' && $_POST['gen']!='f')) {
+  setcookie('gen_error', '1', time() + 24 * 60 * 60);
+  $errors = TRUE;
+}
+else {
+  setcookie('gen_value', $_POST['gen'], time() + 30 * 24 * 60 * 60);
+}
+if (empty($_POST['body']) || ($_POST['body']!='0' && $_POST['body']!='4' && $_POST['body']!='5')) {
+   setcookie('body_error', '1', time() + 24 * 60 * 60);
+   $errors = TRUE;
+}
+else {
+  setcookie('body_value', $_POST['body'], time() + 30 * 24 * 60 * 60);
+}
+
+foreach ($_POST['ability'] as $ability) {
+  if (!is_numeric($ability) || !in_array($ability, [1, 2, 3, 4])) {
+    setcookie('ability_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+    break;
+  }
+}
+if (!empty($_POST['ability'])) {
+  setcookie('ability_value', json_encode($_POST['ability']), time() + 24 * 60 * 60);
+}
+
+if (empty($_POST['biographiya']) || !preg_match('/^[0-9A-Za-z0-9А-Яа-я,\.\s]+$/', $_POST['biographiya'])) {
+    setcookie('biographiya_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+}
+else {
+  setcookie('biographiya_value', $_POST['biographiya'], time() + 30 * 24 * 60 * 60);
+}
+
+if ($errors) {
+  exit();
+}
+
+$user = 'u54409';
+$pass = '3113126';
+$db = new PDO('mysql:host=localhost;dbname=u54409', $user, $pass,
+  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
+
+try {
+  $stmt = $db->prepare("INSERT INTO forma SET name = ?, email=?, birthday=?, sex=?, biographiya=?, limbs=?");
+  $stmt->execute([$_POST['name'], $_POST['email'], $_POST['birthdayear'], $_POST['gen'], $_POST['biographiya'], $_POST['body']]);
+  $app_id = $db->lastInsertId();
+  $stmt = $db->prepare("INSERT INTO abforma SET app_id=?, a_id= ?");
+  foreach ($_POST['ability'] as $ability) {
+    $stmt->execute([$app_id,$ability ]);
+  }
+}
+catch(PDOException $e){
+  print('Error : ' . $e->getMessage());
+  exit();
+}
+
+header('Location: ?save=1');
